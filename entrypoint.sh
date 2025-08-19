c9e71712a9fdbedcef20be2bbc81f7cc6c4f50ea
nudtabza <67319100041@chetupon.ac.th>
2025-08-19 23:52:23 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
+# เปลี่ยน user/group ของ php-fpm.sock ไปเป็น nginx เพื่อให้ Nginx เข้าถึงได้
+# สร้างไฟล์ sock ที่ต้องการ
+mkdir /var/run/php-fpm
+
 # Start PHP-FPM
 php-fpm -F &
 
 # Wait for php-fpm.sock to be created
-while [ ! -S /var/run/php-fpm.sock ]; do
+while [ ! -S /tmp/php-fpm-socket/php-fpm.sock ]; do
     echo "Waiting for php-fpm.sock..."
     sleep 0.1
 done
